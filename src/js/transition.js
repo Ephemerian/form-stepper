f9d18f099ada73b41253d891fb49b5bdf58a3db8
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-03-10 00:32:29 +0200

    add thanks section

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const estimateSection = document.querySelector('.estimate');
     const ratesSection = document.querySelector('.rates');
+    const thanksSection = document.querySelector('.thanks');
     const estimateButton = document.querySelector('.estimate__button-next');
     const editButton = document.querySelector('.rates__edit');
-
+    const personalizeButton = document.querySelector('.rates__button');
+    
     ratesSection.style.display = 'none';
+    thanksSection.style.display = 'none';
 
     estimateButton.addEventListener('click', function() {
         estimateSection.style.display = 'none';
@@ -16,5 +19,10 @@ document.addEventListener('DOMContentLoaded', function() {
         estimateSection.style.display = 'block';
         estimateSection.offsetHeight;
         estimateSection.style.display = 'flex';
+    });
+
+    personalizeButton.addEventListener('click', function() {
+        ratesSection.style.display = 'none';
+        thanksSection.style.display = 'block';
     });
 });
